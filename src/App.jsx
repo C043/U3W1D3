1b7e4da8db544517ac6be913fc7b53713e17612b
Mario Fragnito <mariofragnitoph@gmail.com>
2024-06-26 15:05:31 +0200

    Adds project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+import Home from "./components/Home";
+
+const App = () => <Home />;
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import MyNav from "./MyNav";
+import MyFooter from "./MyFooter";
+import Welcome from "./Welcome";
+import AllTheBooks from "./AllTheBooks";
+
+const Home = () => (
+  <>
+    <MyNav />
+    <Welcome />
+    <AllTheBooks />
+    <MyFooter />
+  </>
+);
+
+export default Home;
