47c46ea96810badcc393c0d72049f2f2c7e49c4c
Mario Fragnito <mariofragnitoph@gmail.com>
2024-06-26 15:13:10 +0200

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
-import Home from "./components/Home";
+import MyNav from "./components/MyNav";
+import Welcome from "./components/Welcome";
+import AllTheBooks from "./components/AllTheBooks";
+import Footer from "./components/MyFooter";
+import MyFooter from "./components/MyFooter";
 
-const App = () => <Home />;
+const App = () => (
+  <>
+    <MyNav />;
+    <Welcome />
+    <AllTheBooks />
+    <MyFooter />
+  </>
+);
 export default App;
